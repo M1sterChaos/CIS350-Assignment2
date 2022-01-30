a0fa494221b31273848bdb209f2f0fb8f35a651e
M1sterChaos <austinbuck70@gmail.com>
2022-01-30 16:59:26 -0600

    Challenge-1

---
+++ Challenge-1/CIS350_Challenge-1/Assets/Challenge 1/Scripts/GameOverBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ScoreManager.gameover = true;
+        }
+    }
+}
---
+++ Challenge-1/CIS350_Challenge-1/Assets/Challenge 1/Scripts/PropellarBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellarBehavior : MonoBehaviour
+{
+    public GameObject propellar;
+    public float pz;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       propellar.transform.Rotate(0f, 0f, pz, Space.World);
+    }
+}
